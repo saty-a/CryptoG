8cd8bdef61dae056c5db27165358bb5687d8b0cc
Shreya Ghosh <shreyaa2410@gmail.com>
2022-10-16 12:13:16 +0530

    Program
    Caesar Cipher program in java.

---
+++ Programs/CaesarCipher.java
@@ -0,0 +1,36 @@
+class CaesarCipher
+{
+    public CaesarCipher() {
+    }
+
+    public static StringBuffer encrypt(String text, int s)
+    {
+        StringBuffer result= new StringBuffer();
+ 
+        for (int i=0; i<text.length(); i++)
+        {
+            if (Character.isUpperCase(text.charAt(i)))
+            {
+                char ch = (char)(((int)text.charAt(i) +
+                                  s - 65) % 26 + 65);
+                result.append(ch);
+            }
+            else
+            {
+                char ch = (char)(((int)text.charAt(i) +
+                                  s - 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        return result;
+    }
+ 
+    public static void main(String[] args)
+    {
+        String text = "ENCRYPTIONTEXT";
+        int s = 5;
+        System.out.println("Text  : " + text);
+        System.out.println("Shift : " + s);
+        System.out.println("Cipher: " + encrypt(text, s));
+    }
+}
